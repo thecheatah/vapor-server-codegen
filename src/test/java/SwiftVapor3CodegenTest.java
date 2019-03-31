61b67196d7f14d3849ec95c427a0dba9a7087727
Ravneet Singh <thecheatah@gmail.com>
2019-03-30 21:24:52 -0400

    Cleaned up SwiftVapor3CodegenTest.java. Removed commented out code and added real comments.

--- src/test/java/SwiftVapor3CodegenTest.java
+++ src/test/java/SwiftVapor3CodegenTest.java
@@ -12,21 +12,20 @@ public class SwiftVapor3CodegenTest {
 
   @Test
   public void simpleTest() throws InterruptedException, IOException {
+    //Build from the test swagger
     SwaggerCodegen.main(new String[]{"generate", "-l", "SwiftVapor3", "-i", "codegen_test.yml", "-o", "./target/test-classes/swift/VaporTestInterface", "--additional-properties", "projectName=VaporTestInterface"});
+    //The previous command runs in a thread. Wait about 4 seconds for the thread to finish
+    //TODO: Make the previous command blocking
     Thread.sleep(4000L);
+
+    //Run swift test to run the test cases in the VaporTestServer
     ProcessBuilder pb = new ProcessBuilder("/usr/bin/swift", "test");
-//    pb.directory(new File("./target/test-classes/swift/VaporTestServer"));
     pb.directory(new File("./src/test/resources/swift/VaporTestServer"));
     pb.redirectOutput(Redirect.INHERIT);
     pb.redirectError(Redirect.INHERIT);
     Process p = pb.start();
     int result = p.waitFor();
 
-//    Process p = Runtime.getRuntime().exec("/usr/bin/swift build", null, new File("target/test-classes/swift/VaporTestServer"));
-//    int result = p.waitFor();
-//    while (p.getInputStream().)
-//    byte[] b = p.getInputStream().readAllBytes();
-//    System.out.write(b);
     System.out.println(result);
     Assert.assertEquals(result, 0);
   }
