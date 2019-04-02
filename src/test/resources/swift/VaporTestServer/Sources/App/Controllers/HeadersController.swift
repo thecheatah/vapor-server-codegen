75829f4bce8b64503dce120b58088c3236233395
Ravneet Singh <thecheatah@gmail.com>
2019-04-02 16:12:06 -0400

    Converted API response to an enum.

---
+++ src/test/resources/swift/VaporTestServer/Sources/App/Controllers/HeadersController.swift
@@ -0,0 +1,8 @@
+//
+//  HeadersController.swift
+//  App
+//
+//  Created by Ravneet Singh on 4/2/19.
+//
+
+import Foundation
---
+++ src/test/resources/swift/VaporTestServer/Tests/AppTests/MultipleResponseCodesTests.swift
@@ -0,0 +1,8 @@
+//
+//  MultipleResponseCodesTests.swift
+//  App
+//
+//  Created by Ravneet Singh on 4/1/19.
+//
+
+import Foundation
