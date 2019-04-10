b597c7a27593992eefb0fa7448bd326da0057ff9
Ravneet Singh <thecheatah@gmail.com>
2019-04-10 08:03:11 -0400

    Changed request: Request in interfaces to req: Request to better align with how Vapor does it.

---
+++ src/test/resources/swift/VaporTestServer/Sources/App/Controllers/AuthenticationController.swift
@@ -0,0 +1,8 @@
+//
+//  AuthenticationController.swift
+//  App
+//
+//  Created by Ravneet Singh on 4/9/19.
+//
+
+import Foundation
---
+++ src/test/resources/swift/VaporTestServer/Sources/App/Middlewares/SecurityMiddleware.swift
@@ -0,0 +1,8 @@
+//
+//  SecurityMiddleware.swift
+//  App
+//
+//  Created by Ravneet Singh on 4/9/19.
+//
+
+import Foundation
