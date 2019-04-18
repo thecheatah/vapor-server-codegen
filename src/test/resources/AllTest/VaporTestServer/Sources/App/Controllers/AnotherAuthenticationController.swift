eb4052ccee19dfae9f248df57b0112be76099ff2
Ravneet Singh <thecheatah@gmail.com>
2019-04-18 19:50:24 -0400

    Changed auth object signature to asAuthenticated user: AuthType. Allows the implementing class to change user to another name.

---
+++ src/test/resources/AllTest/VaporTestServer/Sources/App/Controllers/AnotherAuthenticationController.swift
@@ -0,0 +1,8 @@
+//
+//  AnotherAuthenticationController.swift
+//  App
+//
+//  Created by Ravneet Singh on 4/18/19.
+//
+
+import Foundation
---
+++ src/test/resources/AllTest/VaporTestServer/Sources/App/Middlewares/AnotherSecurityMiddleware.swift
@@ -0,0 +1,8 @@
+//
+//  AnotherSecurityMiddleware.swift
+//  App
+//
+//  Created by Ravneet Singh on 4/18/19.
+//
+
+import Foundation
